26d12eca59105c91395b866ec86e9150fe7b8712
wblyuyang <android_yy@foxmail.com>
2015-11-05 17:44:46 +0800

    wblyuyang

---
+++ src/main/java/com/example/administrator/my/test2.java
@@ -0,0 +1,16 @@
+package com.example.administrator.my;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yy on 2015/11/5.
+ */
+public class test2 extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/main/java/com/example/administrator/my/test3.java
@@ -0,0 +1,14 @@
+package com.example.administrator.my;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by YY on 2015/11/5.
+ */
+public class test3 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
