f68c9320515dacbf3829d726b6d0785bddb927fa
wblyuyang <android_yy@foxmail.com>
2015-11-05 17:35:30 +0800

    test

---
+++ src/main/java/com/example/administrator/my/test.java
@@ -0,0 +1,15 @@
+package com.example.administrator.my;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/11/5.
+ */
+public class test extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
